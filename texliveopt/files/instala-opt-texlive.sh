260c20513377c17c9260813319e8ea6b8a923be9
root <root@ansible.ime.usp.br>
2025-07-03 09:34:41 -0300

    Criada nova role exclusiva para: 	Copiar o script instalador/atualizador de texlive em /root/ 	Copiar o script rstudio_tex.sh para abrir o rstudio apptainer com /opt/texlive Não executa o instalador/atualizador de tex, a exemplo do que já não faz com as imagens de apptainer
    Committer: VirusABC <alopes@ime.usp.br>

--- texliveopt/files/instala-opt-texlive.sh
+++ texliveopt/files/instala-opt-texlive.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #scrips que instala e atualiza texlive-full em /opt/texlive para uso com R e Rstudio do apptainer
 #baseado em https://tug.org/texlive/quickinstall.html e https://tug.org/texlive/doc/tlmgr.html#EXAMPLES
+#Utilize esse script para Instalar ou Atualizar texlive em /opt/texlive pronto para ser utilizado com Apptainer, 
+#Porém:
+#Instalação do zero: cerca de 90 minutos em ótimo hardware. Por enquanto preferimos executar manualmente.
+#Supondo que já tenha uma instalação atualizada em brucutuvii:
+#O mais rápido do zero: ssh root@brucutuvii.ime.usp.br "( cd /opt ; tar --zstd -cf - texlive;)" | tar --zstd -xf -
+#O mais rápido para atualizar: rsync -a root@brucutuvii.ime.usp.br:/opt/texlive /opt/
+#Tar com zstd ~ 1minuto em ótima máquina.
+#Rsync completo: 1m30s (bem menos pra atualização apenas) em ótima máquina.
+#ótima máquina: 1Gbps R7 série 5000 com bastante RAM e NVMe. BrucutuVII Threadripper série 7000 com muita RAM e NVMe.
+#TODO: incluir um "case" para escolher a forma de instalar ou atualizar escolhendo a origem.
+#Sugestão para TODO: Perguntar se quer instalar/atualizar da fonte ou instalar/atualizar de ORIGEM.
 
 #Caminho da pasta com o instalador do texlive
 PASTA_INSTALADOR=/root/texliveinstalador
@@ -9,7 +20,7 @@ INSTALADOR_URL=https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.
 #Pasta de destino da instalação do texlive
 DESTINO_TEXLIVE=/opt/texlive
 #Prefixo da pasta com versão do instalador, é no formato AAAAMMDD, usamos só o começo
-#easter egg: quando chegar nos anos 3000 vai sar algum pau esse script, altere abaixo, hahah
+#easter egg: quando chegar nos anos 3000 vai dar algum pau nesse script, altere abaixo, hahah
 INSTALL_TL_PREFIXO=install-tl-2
 #Lista de erros:
 #1 - não criou $PASTA_INSTALADOR
@@ -57,18 +68,19 @@ remove_instaladores_velhos() {
     	done
 }
 
-echo "Instala ou atualiza TexLive em $DESTINO_TEXLIVE"
+echo "Instala ou atualiza TexLive em $DESTINO_TEXLIVE. Baixando instalador mais recente"
+baixa_instalador
+echo "Removendo instaladores antigos antes da instalação"
+remove_instaladores_velhos
 
 #verifica se NÃO EXISTE nenhuma pasta $INSTALL_TL_PREFIXO*
 if ! [ -d $DESTINO_TEXLIVE/bin/linux/ ]; then
-	echo "A pasta $DESTINO_TEXLIVE/bin/linux/ não existe. Baixando instalador"
-	baixa_instalador
+	echo "A pasta $DESTINO_TEXLIVE/bin/linux/ não existe."
 	echo "Instalando TexLive em $DESTINO_TEXLIVE"
 	instala_tex
 	echo "TexLive instalado em $DESTINO_TEXLIVE"
 else
-    	echo "Possivelmente existe instalação, procedendo com atualização"
+  echo "Possivelmente já existe instalação, procedendo com atualização"
 	atualiza_tex || echo "Erro em atualizar. Verifique se a instalação estava correta"
 fi
-remove_instaladores_velhos
 exit 0
